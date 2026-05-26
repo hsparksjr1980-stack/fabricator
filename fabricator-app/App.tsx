f6249610d9582f6a96f11e11eca3df785c90ea80
Howard Sparks <hsparksjr1980@gmail.com>
2026-05-26 15:50:43 -0500

    Restore Zustand hydration

--- fabricator-app/App.tsx
+++ fabricator-app/App.tsx
@@ -1,12 +1,44 @@
+import { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 
 import { AppNavigator } from './src/navigation/AppNavigator';
+import { useFabricatorStore } from './src/state/useFabricatorStore';
+import { colors } from './src/theme/theme';
 
 export default function App() {
+  const hasLoadedAppData = useFabricatorStore(
+    state => state.hasLoadedAppData
+  );
+
+  const loadAppData = useFabricatorStore(
+    state => state.loadAppData
+  );
+
+  useEffect(() => {
+    loadAppData();
+  }, [loadAppData]);
+
+  if (!hasLoadedAppData) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: colors.black,
+        }}
+      >
+        <StatusBar style="light" backgroundColor={colors.black} />
+        <ActivityIndicator color={colors.orange} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <StatusBar style="light" />
+      <StatusBar style="light" backgroundColor={colors.black} />
       <AppNavigator />
     </NavigationContainer>
   );
