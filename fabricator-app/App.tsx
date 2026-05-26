fba842babadc44ca54e71ac7f72d10ef90b8ceb1
Howard Sparks <hsparksjr1980@gmail.com>
2026-05-26 15:17:54 -0500

    Restore navigation stack

--- fabricator-app/App.tsx
+++ fabricator-app/App.tsx
@@ -1,24 +1,13 @@
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+
+import { AppNavigator } from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text
-        style={{
-          color: 'white',
-          fontSize: 32,
-          fontWeight: '700',
-        }}
-      >
-        Fabricator Works
-      </Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
